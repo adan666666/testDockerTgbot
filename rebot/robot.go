38cc94780056fb635987d574669a70ac8fceb03d
a123123 <shitlaoge@gmail.com>
2024-11-23 22:16:08 +0400

    增加机器人选项功能

--- rebot/robot.go
+++ rebot/robot.go
@@ -22,21 +22,84 @@ func TgRobot(config Conf) {
 	//if err != nil {
 	//	panic(err)
 	//}
+
+	// 存储用户的选择
+	userSelections := make(map[int64][]string)
+
 	u := tgbotapi.NewUpdate(0) //创建了一个新的更新对象 u，用于从 Telegram 服务器获取消息更新。参数 0 表示从最早的未读消息开始获取更新.
 	u.Timeout = 60             //60秒内没有消息更新，就停止轮询，以节约资源
+	//u.Offset = -1              // 跳过旧的更新
 
 	//启动一个定时器 计算到下班还有多长时间
 	go StartTimer(bot, config)
 	// 获取一个监听管道，进行轮询监听飞机消息
-	for update := range bot.GetUpdatesChan(u) {
-		if update.Message == nil { // 忽略任何非消息更新
-			continue
-		}
+	updates := bot.GetUpdatesChan(u)
+	for update := range updates {
+		//if update.Message == nil { // 忽略任何非消息更新 //会把update.CallbackQuery消息过滤掉
+		//	continue
+		//}
 		// 打印收到的消息
-		log.Infof("收到消息==>[%s] %s %v ", update.Message.From.UserName, update.Message.Text, update.Message.Chat.ID)
+		if update.Message != nil {
+			log.Infof("收到消息==>[userName=%s/From.String=%s/ID=%d] [消息是=%s}] [Chat.ID=%v] ", update.Message.From.UserName, update.Message.From.String(), update.Message.From.ID, update.Message.Text, update.Message.Chat.ID) //如果没有设置userName,From.String()可以取到name
+		}
+
+		if update.CallbackQuery != nil { // 用户点击按钮
+			log.Printf("CallbackQuery received: %+v", update.CallbackQuery)
+			callback := update.CallbackQuery
+			chatID := callback.Message.Chat.ID
+			data := callback.Data
+
+			if data == "confirm" { // 用户点击确认
+				selection := userSelections[chatID]
+				if len(selection) == 0 {
+					reply := "你未选择任何选项！"
+					bot.Send(tgbotapi.NewMessage(chatID, reply))
+				} else {
+					reply := fmt.Sprintf("你选择了: %s", strings.Join(selection, ", "))
+					bot.Send(tgbotapi.NewMessage(chatID, reply))
+				}
+				userSelections[chatID] = nil // 清空用户选择
+			} else { // 记录选择
+				userSelections[chatID] = toggleSelection(userSelections[chatID], data)
+				//reply := fmt.Sprintf("当前选择: %s", strings.Join(userSelections[chatID], ", "))
+				//bot.Send(tgbotapi.NewMessage(chatID, reply))
+			}
+
+			// 回答 CallbackQuery
+			bot.Send(tgbotapi.NewCallback(callback.ID, "操作已记录"))
+		}
+
+		if update.Message != nil && update.Message.IsCommand() { // 用户发送命令
+			msg := update.Message
+			chatID := msg.Chat.ID
+
+			switch msg.Command() {
+			case "start":
+				// 发送选项按钮
+				buttons := []tgbotapi.InlineKeyboardButton{
+					tgbotapi.NewInlineKeyboardButtonData("选项1", "option1"),
+					tgbotapi.NewInlineKeyboardButtonData("选项2", "option2"),
+					tgbotapi.NewInlineKeyboardButtonData("选项3", "option3"),
+				}
+				confirmButton := tgbotapi.NewInlineKeyboardButtonData("确认", "confirm")
+
+				keyboard := tgbotapi.NewInlineKeyboardMarkup(
+					tgbotapi.NewInlineKeyboardRow(buttons...),
+					tgbotapi.NewInlineKeyboardRow(confirmButton),
+				)
+
+				reply := tgbotapi.NewMessage(chatID, "请选择选项：")
+				reply.ReplyMarkup = keyboard
+
+				_, err = bot.Send(reply)
+				if err != nil {
+					panic(err)
+				}
+			}
+		}
 
 		// 检查消息是否提到了机器人 或者是命令
-		if update.Message.IsCommand() || strings.Contains(update.Message.Text, "@"+bot.Self.UserName) {
+		if update.Message != nil && (update.Message.IsCommand() || strings.Contains(update.Message.Text, "@"+bot.Self.UserName)) {
 			//if strings.HasPrefix(update.Message.Text, "@bx_xia_Bot") {
 			// 回复消息
 			responseText := "你提到我了吗？我在这里！大佬请指教！"
@@ -72,10 +135,23 @@ func TgRobot(config Conf) {
 			bot.Send(msg)
 		} else {
 			//如果是#号开头，就是我要发到群里的消息
-			if strings.HasPrefix(update.Message.Text, "#") {
+			if update.Message != nil && strings.HasPrefix(update.Message.Text, "#") {
 				msg.Text = update.Message.Text
 				bot.Send(msg)
 			}
 		}
+
 	}
+}
+
+// toggleSelection 用于更新用户的选择
+func toggleSelection(selection []string, item string) []string {
+	for i, v := range selection {
+		if v == item {
+			// 如果已经选中，取消选择
+			return append(selection[:i], selection[i+1:]...)
+		}
+	}
+	// 如果未选中，添加到选择中
+	return append(selection, item)
 }
