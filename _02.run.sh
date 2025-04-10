7df4a9e7d04d686695ad8b6920730aeb4b5cfa6b
a123123 <shitlaoge@gmail.com>
2025-04-10 16:51:00 +0900

    整理 加注释

--- _02.run.sh
+++
@@ -1,5 +0,0 @@
-docker run -itd \
---privileged=true \
--v /etc/localtime:/etc/localtime:ro \
---name tgbot -p 5000:5000 tgbot:v1
-
--- _03.compose.sh
+++
@@ -1,4 +0,0 @@
-docker-compose down --rmi all
-#--build参数 它适合在您对 Dockerfile 或服务配置进行了更改后使用。
-#docker-compose up --build
-docker-compose up
